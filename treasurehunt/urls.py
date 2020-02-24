5acf67ed09d1769b29feac9224008ef1e0542eb9
myb204 <myb204@exeter.ac.uk>
2020-02-24 16:41:57 +0000

    Removed task_two page.

--- treasurehunt/urls.py
+++ treasurehunt/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.home, name='treasurehunt-home'),
     path('leaderboard/', views.leaderboard, name='treasurehunt-leaderboard'),
     path('task_one/', views.task_one, name='treasurehunt-task_one'),
-    path('task_two/', views.task_two, name='treasurehunt-task-two'),
     path('end/', views.end, name='treasurehunt-end'),
 ]
--- treasurehunt/views.py
+++ treasurehunt/views.py
@@ -11,8 +11,5 @@ def leaderboard(request):
 def task_one(request):
     return render(request, 'treasurehunt/task_one.html', {'title': 'One'})
 
-def task_two(request):
-    return render(request, 'treasurehunt/task_two.html', {'title': 'Two'})
-
 def end(request):
     return render(request, 'treasurehunt/end.html', {'title': 'end'})
