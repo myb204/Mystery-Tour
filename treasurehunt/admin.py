b39e3de0f3fc51391e3c33cc1eb4bd73bc651d13
myb204 <myb204@exeter.ac.uk>
2020-02-25 03:41:17 +0000

    Registered models onto admin page. Allows you to see models on admin page and add dummy data.

--- treasurehunt/admin.py
+++ treasurehunt/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Clue, Task, Location, Route, RouteLocationMapping, Team, Game
 
-# Register your models here.
+admin.site.register(Clue)
+admin.site.register(Task)
+admin.site.register(Location)
+admin.site.register(Route)
+admin.site.register(RouteLocationMapping)
+admin.site.register(Team)
+admin.site.register(Game)
