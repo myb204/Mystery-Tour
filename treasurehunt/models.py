be50dfb3cc247b430fd6adc08aafeab106977f52
trm <moore7980@gmail.com>
2020-02-25 02:45:06 +0000

    Interaction between pages finalised. Bug Fixes

--- treasurehunt/models.py
+++ treasurehunt/models.py
@@ -6,33 +6,50 @@ class Clue(models.Model):
     clueText = models.CharField(max_length=255)
     imageFilePath = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.clueText
+
 class Task(models.Model):
     taskID = models.AutoField(primary_key=True)
     taskText = models.CharField(max_length=255)
     taskAnswer = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.taskText
+
 class Location(models.Model):
     locationID = models.AutoField(primary_key=True)
-    clueID = models.ForeignKey(Clue, on_delete=models.CASCADE)
-    taskID = models.ForeignKey(Task, on_delete=models.CASCADE)
+    clueID = models.ForeignKey(Clue, on_delete=models.PROTECT)
+    taskID = models.ForeignKey(Task, on_delete=models.PROTECT)
     name = models.CharField(max_length=20)
     address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 class Route(models.Model):
     routeID = models.AutoField(primary_key=True)
 
+    def __str__(self):
+        return self.routeID
+
 class RouteLocationMapping(models.Model):
-    routeID = models.ForeignKey(Route, on_delete=models.CASCADE)
-    locationID = models.ForeignKey(Location, on_delete=models.CASCADE)
+    routeID = models.ForeignKey(Route, on_delete=models.PROTECT)
+    locationID = models.ForeignKey(Location, on_delete=models.PROTECT)
 
 class Team(models.Model):
     teamID = models.AutoField(primary_key=True)
     teamName = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.teamName
+
 class Game(models.Model):
     gameID = models.AutoField(primary_key=True)
-    teamID = models.ForeignKey(Team, on_delete=models.CASCADE)
-    routeID = models.ForeignKey(Route, on_delete=models.CASCADE)
+    teamID = models.ForeignKey(Team, on_delete=models.PROTECT)
+    routeID = models.ForeignKey(Route, on_delete=models.PROTECT)
     date = models.DateTimeField(default=timezone.now)
     score = models.IntegerField()
 
+    def __str__(self):
+        return self.gameID
--- treasurehunt/views.py
+++ treasurehunt/views.py
@@ -9,11 +9,17 @@ def start(request):
     return render(request, 'treasurehunt/start.html')
 
 def leaderboard(request):
-    return render(request, 'treasurehunt/leaderboard.html', {'title': 'leaderbaord'})
+    return render(request, 'treasurehunt/leaderboard.html', {'title': 'Leaderboard'})
+
+def clue(request):
+    return render(request, 'treasurehunt/clue.html')
 
 def task_one(request):
-    return render(request, 'treasurehunt/task_one.html', {'title': 'One'})
+    return render(request, 'treasurehunt/task_one.html', {'title': 'Task'})
+
+def info(request):
+    return render(request, 'treasurehunt/info.html', {'title': 'Info'})
 
 def end(request):
-    return render(request, 'treasurehunt/end.html', {'title': 'end'})
+    return render(request, 'treasurehunt/end.html', {'title': 'End'})
 
