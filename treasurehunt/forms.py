ee4ae34d809775a76143f02cc44f3690dd36f469
jayodedra <jo367@exeter.ac.uk>
2020-02-25 21:22:12 +0000

    Changed database Form to a ModelForm

--- treasurehunt/forms.py
+++ treasurehunt/forms.py
@@ -1,2 +1,23 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
+from treasurehunt.models import Team
+
+
+class teamForm(forms.ModelForm):
+
+    def clean_teamMembers(self):
+        data = self.cleaned_data['teamMembers']
+        if data < 1:
+            raise ValidationError('Invalid Number of Players. Too Few')
+
+        if data > 6:
+            raise ValidationError('Invalid Number of Players. Too Many')
+
+        return data
+
+    class Meta:
+        model = Team
+        fields = ['teamName', 'teamMembers']
+        labels = {'teamName': 'Team Name',
+                  'teamMembers': 'Number of Players (1-6)'}
