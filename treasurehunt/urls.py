2ff9e027136f544dc99c2294f55b0685dfecdea0
Jaggeroo <60703599+Jaggeroo@users.noreply.github.com>
2020-02-25 21:15:33 +0000

    Adding missing URL patterns

--- treasurehunt/urls.py
+++ treasurehunt/urls.py
@@ -4,9 +4,15 @@ from . import views
 urlpatterns = [
     path('', views.home, name='treasurehunt-home'),
     path('start/', views.start, name='treasurehunt-start'),
-    path('leaderboard/', views.leaderboard, name='treasurehunt-leaderboard'),
+    path('leaderboard/', views.leaderboard.as_view(), name='treasurehunt-leaderboard'),
+    path('help/', views.help, name='treasurehunt-help'),
+    path('about/', views.about, name='treasurehunt-about'),
+    path('faqs/', views.faqs, name='treasurehunt-faqs'),
+    path('qr/', views.qr, name='treasurehunt-qr'),
+    path('assistance/', views.assistance, name='treasurehunt-assistance'),
+    path('howtoplay/', views.howtoplay, name='treasurehunt-howtoplay'),
     path('clue/', views.clue, name='treasurehunt-clue'),
-    path('task_one/', views.task_one, name='treasurehunt-task_one'),
+    path('task/', views.task, name='treasurehunt-task'),
     path('info/', views.info, name='treasurehunt-info'),
     path('end/', views.end, name='treasurehunt-end'),
 ]
