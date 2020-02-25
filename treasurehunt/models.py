1343d35008f2bd7d0d0e3af4803659e4fbb29115
ejm235 <ejm235@exeter.ac.uk>
2020-02-25 20:40:19 +0000

    models.py - implemented

--- treasurehunt/models.py
+++ treasurehunt/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Clue(models.Model):
+    clueID = models.IntegerField(primary_key=True)
+    clueText = models.CharField(max_length=255, default="NoClue")
+    imageFilePath = models.CharField(max_length=255, default="NoPath")
+
+    def __str__(self):
+        return self.clueText
+
+
+class Task(models.Model):
+    taskID = models.IntegerField(primary_key=True)
+    taskText = models.CharField(max_length=255, default="NoTask")
+    taskAnswer = models.CharField(max_length=255, default="NoAnswer")
+
+    def __str__(self):
+        return self.taskText
+
+
+class Location(models.Model):
+    locationID = models.IntegerField(primary_key=True)
+    clueID = models.ForeignKey(Clue, on_delete=models.PROTECT)
+    taskID = models.ForeignKey(Task, on_delete=models.PROTECT)
+    name = models.CharField(max_length=20, default="NoName")
+    address = models.CharField(max_length=255, default="NoAddress")
+    description = models.CharField(max_length=255, default="NoDescription")
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    routeID = models.IntegerField(primary_key=True)
+    routeName = models.CharField(max_length=20, default='NoRoute')
+
+    def __str__(self):
+        return str(self.routeID)
+
+
+class Team(models.Model):
+    teamID = models.IntegerField(primary_key=True)
+    teamName = models.CharField(max_length=20)
+    teamMembers = models.IntegerField()
+    routeID = models.ForeignKey(Route, on_delete=models.PROTECT, default=1)
+    date = models.DateTimeField(default=timezone.now)
+    score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.teamName
+
+    class Meta:
+        ordering = ['-score']
