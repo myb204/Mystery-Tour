d7fd3c039df02ebabbb96c384f619eeaf33d261a
11saneyg <gs492@exeter.ac.uk>
2020-02-25 20:52:11 +0000

    Implemented view functions for all pages.

--- treasurehunt/views.py
+++ treasurehunt/views.py
@@ -1,25 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views import generic
 
-# Create your views here.
+from .forms import teamForm
+from .models import Team
+
 
 def home(request):
     return render(request, 'treasurehunt/home.html')
 
-def start(request):
-    return render(request, 'treasurehunt/start.html')
 
-def leaderboard(request):
-    return render(request, 'treasurehunt/leaderboard.html', {'title': 'Leaderboard'})
+def help(request):
+    return render(request, 'treasurehunt/help.html')
+
+
+def about(request):
+    return render(request, 'treasurehunt/about.html')
+
+
+def faqs(request):
+    return render(request, 'treasurehunt/faqs.html')
+
+
+def assistance(request):
+    return render(request, 'treasurehunt/assistance.html')
+
+
+def start(request):
+    if request.method == 'POST':
+        form = teamForm(request.POST)
+        if form.is_valid():
+            form.save()
+            teamName = form.cleaned_data.get('teamName')
+            messages.success(request, f'Team created: {teamName}')
+            return redirect('treasurehunt-howtoplay')
+    else:
+        form = teamForm()
+
+    return render(request, 'treasurehunt/start.html', {'form': form})
+
+
+class leaderboard(generic.ListView):
+    model = Team
+    template_name = 'treasurehunt/leaderboard.html'
+
+def howtoplay(request):
+    return render(request, 'treasurehunt/howtoplay.html', {'title': 'How to Play'})
+
 
 def clue(request):
     return render(request, 'treasurehunt/clue.html')
 
-def task_one(request):
-    return render(request, 'treasurehunt/task_one.html', {'title': 'Task'})
+
+def task(request):
+    return render(request, 'treasurehunt/task.html', {'title': 'Task'})
+
+def qr(request):
+    return render(request, 'treasurehunt/qr.html', {'title': 'QR'})
 
 def info(request):
     return render(request, 'treasurehunt/info.html', {'title': 'Info'})
 
+
 def end(request):
     return render(request, 'treasurehunt/end.html', {'title': 'End'})
-
