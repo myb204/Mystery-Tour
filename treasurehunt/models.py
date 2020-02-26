b8da1f41d1e55f281fb508a9db81323ce374c232
trm <moore7980@gmail.com>
2020-02-26 22:05:17 +0000

    Bug Fix

--- treasurehunt/models.py
+++ treasurehunt/models.py
@@ -33,7 +33,7 @@ class Location(models.Model):
 
 
 class Team(models.Model):
-    teamID = models.IntegerField(primary_key=True)
+    teamID = models.AutoField(primary_key=True)
     teamName = models.CharField(max_length=20)
     teamMembers = models.IntegerField(default=0)
     routeID = models.IntegerField(default=1)
