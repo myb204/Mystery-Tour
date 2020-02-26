1bf9dffc6b64a152cf5d66f5e796e3e35bd17aaa
ejm235 <ejm235@exeter.ac.uk>
2020-02-26 09:31:13 +0000

    hgfd

--- treasurehunt/models.py
+++ treasurehunt/models.py
@@ -33,8 +33,8 @@ class Location(models.Model):
 
 
 class Team(models.Model):
-    teamID = models.IntegerField(primary_key=True)
-    teamName = models.CharField(max_length=20)
+    #teamID = models.IntegerField(primary_key=True)
+    teamName = models.CharField(primary_key=True, max_length=20)
     teamMembers = models.IntegerField(default=0)
     routeID = models.IntegerField(default=1)
     date = models.DateTimeField(default=timezone.now)
