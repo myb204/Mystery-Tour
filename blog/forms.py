a402c924ee1cdea21e6199d8dc134bd8f7b59c65
myb204 <myb204@exeter.ac.uk>
2020-03-12 09:07:36 +0000

    Code comments.

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,9 @@
 from .models import Comment
 from django import forms
 
+"""
+form class that generates a model form for comments
+"""
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
